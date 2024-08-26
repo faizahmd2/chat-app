17459f4216d1d9bc51a348c8e241dad83b8f0357
Faiz Ahmad <faizahmd931@gmail.com>
2024-08-26 05:06:08 -0700

    changes variable key

--- app/services/instantdbService.ts
+++ app/services/instantdbService.ts
@@ -1,7 +1,6 @@
 import { init, tx, id } from '@instantdb/react';
 import { Schema } from '../../types/schema';
 
-// Visit https://instantdb.com/dash to get your APP_ID :)
-const db = init<Schema>({ appId: process.env.NEXT_PUBLIC_APP_ID })
+const db = init<Schema>({ appId: process.env.NEXT_INSTANT_DB_APP_ID })
 
 export { db, tx, id };
